f3ca5774aed2691886d28975f6b4c11f38173b85
sunny lalwani <sunny@Mac.lan>
2025-10-11 14:13:48 +0530

    relation added in api

--- src/modules/api/api.module.ts
+++ src/modules/api/api.module.ts
@@ -4,12 +4,14 @@ import { ApiService } from './api.service';
 import { BloodGroupTypesModule } from 'src/modules/bloodgroup-types/bloodgroup-types.module';
 import { ClothingSizeTypesModule } from 'src/modules/clothingsize-types/clothingsize-types.module';
 import { ShoesizeTypesModule } from 'src/modules/shoesize-types/shoesize-types.module';
+import { RelationTypesModule } from 'src/modules/relation-types/relation-types.module';
 
 @Module({
 	imports: [
 		BloodGroupTypesModule,
 		ClothingSizeTypesModule,
 		ShoesizeTypesModule,
+		RelationTypesModule,
 	],
 	controllers: [ApiController],
 	providers: [ApiService],
--- src/modules/api/api.service.ts
+++ src/modules/api/api.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { BloodGroupTypesService } from 'src/modules/bloodgroup-types/bloodgroup-types.service';
 import { ClothingSizeTypesService } from 'src/modules/clothingsize-types/clothingsize-types.service';
 import { ShoesizeTypesService } from 'src/modules/shoesize-types/shoesize-types.service';
+import { RelationTypesService } from 'src/modules/relation-types/relation-types.service';
 
 @Injectable()
 export class ApiService {
@@ -9,14 +10,16 @@ export class ApiService {
 		private readonly bloodGroupTypesService: BloodGroupTypesService,
 		private readonly clothingSizeTypesService: ClothingSizeTypesService,
 		private readonly shoesizeTypesService: ShoesizeTypesService,
+		private readonly relationTypesService: RelationTypesService,
 	) {}
 
 	async getDropdown() {
-		const [blood_groups, clothing_sizes, shoe_sizes] = await Promise.all([
+		const [blood_groups, clothing_sizes, shoe_sizes, relation_types] = await Promise.all([
 			this.bloodGroupTypesService.getAll(),
 			this.clothingSizeTypesService.getAll(),
 			this.shoesizeTypesService.getAll(),
+			this.relationTypesService.getAll(),
 		]);
-		return { blood_groups, clothing_sizes, shoe_sizes };
+		return { blood_groups, clothing_sizes, shoe_sizes, relation_types };
 	}
 }
